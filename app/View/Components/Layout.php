f4d5738348650f83d0b4746ba83ead29cb3889fe
Richard Leek <richard@richardleek.com>
2025-12-19 16:48:32 +0100

    Add icons

---
+++ app/View/Components/Layout.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Layout extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $title = '',
+        public string $description = 'Richard Leek\'s development blog',
+    ) {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View
+    {
+        return view('components.layout');
+    }
+}
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ? (config('app.name', 'Laravel') . ' - ' . $title) : config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="{{ $description ?? 'Richard Leek\'s development blog' }}">
+    <link rel="icon" sizes="32x32" href="/img/favicon-32x32.png" type="image/png">
+    <link rel="icon" sizes="16x16" href="/img/favicon-16x16.png" type="image/png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+</head>
+
+<body>
+    <div class="flex content-center items-center min-h-screen w-full">
+        <main class="w-full min-h-75vh p-4em">
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
+</html>
