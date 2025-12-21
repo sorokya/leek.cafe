745628972a35769063dd5c788f29fa4c08890488
Richard Leek <richard@richardleek.com>
2025-12-21 11:55:35 +0100

    Make password null by default

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,18 +3,12 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
     /**
      * Define the model's default state.
      *
@@ -25,7 +19,6 @@ class UserFactory extends Factory
         return [
             'username' => fake()->unique()->userName(),
             'name' => fake()->name(),
-            'password' => static::$password ??= Hash::make('password'),
         ];
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('username')->unique();
             $table->string('name');
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->timestamps();
         });
     }
