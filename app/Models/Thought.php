e216ce060998945ff0c300aadc2b535eca342e0f
Richard Leek <richard@richardleek.com>
2026-01-03 14:00:20 +0100

    Add thoughts schema and model

---
+++ app/Models/Thought.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int $content_id
+ * @property-read \App\Models\Content|null $content
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Thought newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Thought newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Thought query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Thought whereContentId($value)
+ *
+ * @mixin \Eloquent
+ */
+final class Thought extends Model
+{
+    /** @use HasFactory<\Database\Factories\ThoughtFactory> */
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'content_id',
+    ];
+
+    /** @return BelongsTo<Content, $this> */
+    public function content(): BelongsTo
+    {
+        return $this->belongsTo(Content::class, 'content_id');
+    }
+}
