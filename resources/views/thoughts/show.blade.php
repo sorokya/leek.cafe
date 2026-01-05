501c1ab017daa696c54c32fbd850840ca3fd630c
Richard Leek <richard@richardleek.com>
2026-01-05 11:30:38 +0100

    Cleanup embed styling

--- resources/views/thoughts/show.blade.php
+++ resources/views/thoughts/show.blade.php
@@ -32,20 +32,35 @@
             </header>
 
             <div data-thought-view>
-                <div class="thoughts-item__content content">
-                    {!! $renderedBody !!}
-                </div>
-
-                @if ($content->embedImages->isNotEmpty())
-                    <div class="embed-gallery">
-                        @foreach ($content->embedImages as $image)
-                            <a class="embed-thumb" href="{{ $image->getUrl() }}" target="_blank" rel="noopener">
-                                <img src="{{ $image->getThumbnailUrl() }}" alt="" loading="lazy"
-                                    decoding="async" />
-                            </a>
-                        @endforeach
+                <div class="thoughts-item__content">
+                    <div class="content">
+                        {!! $renderedBody !!}
                     </div>
-                @endif
+
+                    @if ($content->embedImages->isNotEmpty())
+                        <div class="embed-gallery">
+                            @foreach ($content->embedImages as $image)
+                                <div class="embed-item">
+                                    <a class="embed-thumb" href="{{ $image->getUrl() }}" target="_blank" rel="noopener">
+                                        <img src="{{ $image->getThumbnailUrl() }}" alt="" loading="lazy"
+                                            decoding="async" />
+
+                                        @if (strtolower((string) $image->extension) === 'mp4')
+                                            <span class="embed-play" aria-hidden="true">
+                                                <span class="embed-play__icon">
+                                                    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                                        <path
+                                                            d="M8 5.5v13a1 1 0 0 0 1.52.85l10-6.5a1 1 0 0 0 0-1.7l-10-6.5A1 1 0 0 0 8 5.5Z" />
+                                                    </svg>
+                                                </span>
+                                            </span>
+                                        @endif
+                                    </a>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endif
+                </div>
             </div>
 
             @auth
